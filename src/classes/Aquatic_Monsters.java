cf7c566d4b19acea8283f758280f8907a3c63cea
Armand Cuvelier <armandcuvelier@gmail.com>
2025-11-13 00:32:15 +0100

    Creation of the monsters class

---
+++ src/classes/Aquatic_Monsters.java
@@ -0,0 +1,28 @@
+package classes;
+
+import java.util.Random;
+
+/*
+ * Represents a Aquatic Monster who herits of Monster
+ * His type will be Aquatic
+ */
+
+public class Aquatic_Monsters extends Monsters {
+
+    /*
+    *Create a Monster with the statistics of the type Aqua
+    *who impact the statistics
+    */
+    public Aquatic_Monsters(int level_max,int level_min){
+        Random r = new Random();
+
+        this.type = "aquatic";
+        this.weakness = "terrestrial";
+
+        this.atq = r.nextDouble(level_min+2,level_max+3);
+        this.hp = r.nextDouble(level_min,level_max-4);
+        this.speed = r.nextDouble(level_min,level_max);
+        this.def = r.nextDouble(level_min+2,level_max+3);
+    }
+
+}
---
+++ src/classes/Monsters.java
@@ -0,0 +1,37 @@
+package classes;
+/*
+ Represents a class Monsters with 
+ */
+public abstract class Monsters {
+
+    //Information of the monsters
+    
+    double atq;
+    double def;
+    double hp;
+    double speed;
+
+    String name;
+
+    String weakness;
+
+    //Type will impact the stats of the monsters but more the weakness
+    String type;
+    
+    //Return the statistiques of the monster
+    public double [] getStatistics(){
+        double [] statistiques = {atq,def,hp,speed};
+        return statistiques;
+    }
+
+    //Return the informations of the monster
+    public String [] getInformations(){
+        String [] informations={name,weakness,type};
+        return informations;
+    }
+
+    //Set the name of the monster
+    public void setName(String name){
+        this.name = name;
+    }
+}
